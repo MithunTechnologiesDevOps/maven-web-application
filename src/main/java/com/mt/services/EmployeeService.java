626358ab9c497bdce67325c867d928ebdd2e8292
Ubuntu <ubuntu@ip-172-31-4-31.ap-south-1.compute.internal>
2020-11-08 06:53:37 +0000

    modified the file

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -22,9 +22,9 @@ public class EmployeeService {
 			throws JSONException {
 
 		JSONObject js = new JSONObject();
-		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
-		js.put("DOB", "08-Nov-2011");
+		js.put("Name", "Software Solution");
+		js.put("Calling Name", "Sanjay");
+		js.put("DOB", "05-JUNE-2020");
 		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
 		js.put("Places he like", "His native place");
 
