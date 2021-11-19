f01383b461212fbf0a0b1698910ad14163258f2b
akkanavamsi <88872385+akkanavamsi@users.noreply.github.com>
2021-11-19 12:16:20 +0530

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -26,7 +26,7 @@ public class EmployeeService {
 		js.put("Calling Name", "Mithun");
 		js.put("DOB", "08-Nov-2011");
 		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
-		js.put("Places he like", "His native place");
+		js.put("Places he like", "His first native place");
 
 		return js.toString();
 }
