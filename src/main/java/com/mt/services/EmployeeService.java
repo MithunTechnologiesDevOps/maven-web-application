eb44840b555b2d6799300ca4a0aeb088187ceb7a
shashikiran76 <122614511+shashikiran76@users.noreply.github.com>
2023-01-19 22:30:36 +0530

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -23,7 +23,7 @@ public class EmployeeService {
 
 		JSONObject js = new JSONObject();
 		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
+		js.put("Calling Name", "shashikiran");
 		js.put("DOB", "08-Nov-2011");
 		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
 		js.put("Places he like", "His native place");
