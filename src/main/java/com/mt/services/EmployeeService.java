36960c7084494134f206a8b9bf8fff3749e3c977
Vachrushka <vasya.tishhenko.2001@mail.ru>
2025-07-14 08:25:51 +0000

    commit-ai-1752481551.8000355-6ab754ee75cbc79f0a2bd3d39adbda75

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -11,23 +11,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * Класс EmployeeService отвечает за обработку запросов, связанных с сотрудниками.
+ */
 @Controller
 @RequestMapping("/employee")
 public class EmployeeService {
 
-	
-	@RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
-	@ResponseBody
-	String uploadImage(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
-			throws JSONException {
+    /**
+     * Обрабатывает GET-запрос для получения деталей сотрудника.
+     *
+     * @param request  объект запроса, содержащий информацию о запросе
+     * @param response объект ответа, используемый для формирования ответа
+     * @param httpSession объект сессии, который может использоваться для хранения информации о сессии
+     * @return строка в формате JSON с деталями сотрудника
+     * @throws JSONException если возникает ошибка при создании JSON-объекта
+     */
+    @RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
+    @ResponseBody
+    String uploadImage(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
+            throws JSONException {
 
-		JSONObject js = new JSONObject();
-		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
-		js.put("DOB", "08-Nov-2011");
-		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
-		js.put("Places he like", "His native place");
+        // Создаем новый JSON-объект для хранения данных о сотруднике
+        JSONObject js = new JSONObject();
+        js.put("Name", "Mithun Technologies");
+        js.put("Calling Name", "Mithun");
+        js.put("DOB", "08-Nov-2011");
+        js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
+        js.put("Places he like", "His native place");
 
-		return js.toString();
-}
+        // Возвращаем строку JSON, содержащую информацию о сотруднике
+        return js.toString();
+    }
 }
